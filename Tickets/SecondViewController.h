81405ab5ab9857f3c8dad99ee6a0a312bab697d3
Booharin <31410351+Booharin@users.noreply.github.com>
2018-01-30 20:43:50 +0300

    SecondViewController added

---
+++ Tickets/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Tickets
+//
+//  Created by Александр on 30.01.2018.
+//  Copyright © 2018 Александр. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
